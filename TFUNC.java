17486f40409de9537394864eb6e38fc6c5ed0678
korneliusnababan66-wq <korneliusnababan66@gmail.com>
2025-11-11 21:13:12 +0700

    Selesai
    Selesai

--- TFUNC.java
+++ TFUNC.java
@@ -1,6 +1,7 @@
 //12S25013 kornelius Nababan
 import java.util.*;
 import java.lang.Math;
+
 public class TFUNC {
     private static Scanner input = new Scanner(System.in);
 
@@ -11,5 +12,17 @@ public class TFUNC {
         hasil = pertambahan(n);
         System.out.println("Bilangan yang dimasukkan : " + n);
         System.out.println("Hasil perhitungan rekursif : " + hasil);
+    }
+    
+    public static int pertambahan(int n) {
+        int hasil;
+
+        if (n == 1) {
+            hasil = 1;
+        } else {
+            hasil = pertambahan(n - 1) + n;
+        }
+        
+        return hasil;
     }
 }
